bf6fc6a3877a82493dd357b80469d65afb835f4e
Julia Georgiew <java2011@me.com>
2024-01-03 20:28:36 +0100

    views

--- videoflix/videoflixbackend/urls.py
+++ videoflix/videoflixbackend/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import LoginView
+from .views import LoginView, SignupView, VideoView
 
 router = routers.DefaultRouter()
 
 urlpatternd = [
     
     path('login/',  LoginView.as_view(), name='login'),
+    path("signup/", SignupView.as_view(), name='signup'),
+    path('videos/<int:pk/', VideoView.as_view(), name='video_detail'),
     path("", include(router.urls))
 ]
--- videoflix/videoflixbackend/views.py
+++ videoflix/videoflixbackend/views.py
@@ -4,8 +4,15 @@ from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import status as http_status
+from rest_framework import status
+from rest_framework import viewsets
 from django.views.decorators.cache import cache_page
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from .serializers import VideoSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Video
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
@@ -18,13 +25,55 @@ class LoginView(APIView):
         user = authenticate(username=email, password=password)
        
         if user:
-            print('Hi Schnecki')
-            # token, created = Token.objects.get_or_create(user=user)
-            # print(token)
-            # return Response({
-            #     'token': token.key,
-            #     'user_id': user.pk
+            print('Hi')
+            token, created = Token.objects.get_or_create(user=user)
+            print(token)
+            return Response({
+                'token': token.key,
+                'user_id': user.pk
                 
-            # })
+            })
         else:
             return Response({'detail': 'Invalid credentials'}, status=http_status.HTTP_400_BAD_REQUEST)
+ 
+class SignupView(APIView):
+    
+    def post(self, request):
+        first_name = request.data.get('first_name')
+        last_name = request.data.get('last_name')
+        email = request.data.get('username')
+        password = request.data.get('password')
+
+        if not all([first_name, last_name, email, password]):
+            return Response({'error': 'Alle Felder müssen ausgefüllt sein.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=email).exists():
+            return Response({'error': 'Dieser Benutzer existiert bereits.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=email, email=email, password=password, first_name=first_name, last_name=last_name)
+        
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key}, status=status.HTTP_201_CREATED)       
+        
+
+
+class VideoView(viewsets.ModelViewSet):
+    
+    serializer_class = VideoSerializer
+    permission_classes = [IsAuthenticated]
+ 
+    @cache_page(CACHE_TTL)
+    def get_queryset(self):
+        current_user = self.request.user #eingloggten user holen
+        if current_user.is_authenticated:
+            return Video.objects.filter(author=current_user)
+        return Video.objects.none()
+    
+    
+    @cache_page(CACHE_TTL)
+    def video_detail(request, movie_id):
+        try:
+            movie = Video.objects.get(pk=movie_id)
+            return movie
+        except Video.DoesNotExist:
+             return Response({'error': 'Video existiert nicht'}, status=status.HTTP_400_BAD_REQUEST)
