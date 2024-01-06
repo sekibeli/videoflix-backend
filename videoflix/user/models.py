0b9dd14611e9b63f66c57d274b114e72a116f076
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-01-06 14:27:43 +0100

    installed Django-Allauth and set configurations in settings.py and urls.py

--- videoflix/user/models.py
+++ videoflix/user/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    custom = models.CharField(max_length=1000, default='')
+    is_verified = models.BooleanField(default=False)
     phone = models.CharField(max_length=20, default='')
     adress = models.CharField(max_length=150, default='')
