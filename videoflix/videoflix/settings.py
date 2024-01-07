14c8ec9299008f26d060c8528bd0e9ff2e031469
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-01-07 11:30:49 +0100

    Changed database data in settings.py

--- videoflix/videoflix/settings.py
+++ videoflix/videoflix/settings.py
@@ -130,32 +130,32 @@ WSGI_APPLICATION = "videoflix.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         # "NAME": BASE_DIR / "db.sqlite3",
-#          "NAME": "videodb",
-#         "USER": "jgeo",
-#         "PASSWORD": "justAPW",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-        
-#     }
-# }
-
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         # "NAME": BASE_DIR / "db.sqlite3",
          "NAME": "videodb",
-        "USER": "postgres",
-        "PASSWORD": "Primax21!",
+        "USER": "jgeo",
+        "PASSWORD": "justAPW",
         "HOST": "localhost",
         "PORT": "5432",
         
     }
 }
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         # "NAME": BASE_DIR / "db.sqlite3",
+#          "NAME": "videodb",
+#         "USER": "postgres",
+#         "PASSWORD": "Primax21!",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+        
+#     }
+# }
 
 
 # Password validation
