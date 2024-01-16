34395479870a49bb5e98ced14e973541b5af8d21
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-01-16 06:25:14 +0100

    Replaced the whole buisness logic about user into the view of the user app

--- videoflix/videoflix/urls.py
+++ videoflix/videoflix/urls.py
@@ -18,13 +18,15 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from videoflixbackend.views import (
-    LoginView, 
+
+from user.views import (
     SignupView, 
-    LogoutView, 
     VerifyEmailView,
+    LoginView,
+    LogoutView,
     LoggeduserView
 )
+
 
 
 urlpatterns = [
