da8ab9aaa233ccfd1514c203eea0db263d96794a
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2024-02-18 23:04:40 +0100

    Merge branch 'development' into feature/video-likes

--- videoflix/videoflixbackend/views.py
+++ videoflix/videoflixbackend/views.py
@@ -36,7 +36,9 @@ class VideoSearchView(APIView):
             )
         else:
             videos = Video.objects.all()
-        serializer = VideoSerializer(videos, many=True)
+        
+        # Passen Sie den Kontext beim Erstellen des Serializers an
+        serializer = VideoSerializer(videos, many=True, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
